e7209f3305217ce361bc62a2cbe2f8c1dec57ae5
Saikiran Maddela <saikiran.m00@gmail.com>
2022-11-15 16:16:03 -0600

    Thresholding atleast runs

--- src/train_test.py
+++ src/train_test.py
@@ -16,25 +16,31 @@ def train_models(data, labels):
             pred = model.predict(data)
             # TODO better thresholding?
             if isinstance(constraint, DemographicParity):
-                m_pred = pred[data['Gender'] == 1]
-                f_pred = pred[data['Gender'] == 0]
-                sub_m_pred = np.sort(m_pred[labels == 1])
-                sub_f_pred = np.sort(f_pred[labels == 1])
-                m_threshold = sorted_pred[int(sub_m_pred.shape[0] * 0.18)]
-                f_threshold = sorted_pred[int(sub_f_pred.shape[0] * 0.18)]
+                m_idx = data['Gender'] == 1
+                f_idx = data['Gender'] == 0
+                m_pred = pred[m_idx]
+                f_pred = pred[f_idx]
+                sub_m_pred = np.sort(m_pred[labels[m_idx] == 1])
+                sub_f_pred = np.sort(f_pred[labels[f_idx] == 1])
+                m_threshold = sub_m_pred[int(sub_m_pred.shape[0] * 0.18)]
+                f_threshold = sub_f_pred[int(sub_f_pred.shape[0] * 0.18)]
                 thresholds.append([m_threshold, m_threshold, f_threshold, f_threshold])
             elif isinstance(constraint, EqualizedOdds):
-                m_pred = pred[data['Gender'] == 1]
-                f_pred = pred[data['Gender'] == 0]
-                sub_m_pred = np.sort(m_pred[labels == 1])
-                sub_f_pred = np.sort(f_pred[labels == 1])
-                m_threshold1 = sorted_pred[int(sub_m_pred.shape[0] * 0.18)]
-                f_threshold1 = sorted_pred[int(sub_f_pred.shape[0] * 0.18)]
+                m_idx = data['Gender'] == 1
+                f_idx = data['Gender'] == 0
+                m_pred = pred[m_idx]
+                f_pred = pred[f_idx]
+                sub_m_pred = np.sort(m_pred[labels[m_idx] == 1])
+                sub_f_pred = np.sort(f_pred[labels[f_idx] == 1])
+                m_threshold1 = sub_m_pred[int(sub_m_pred.shape[0] * 0.18)]
+                f_threshold1 = sub_f_pred[int(sub_f_pred.shape[0] * 0.18)]
 
-                sub_m_pred = np.sort(m_pred[labels == 0])
-                sub_f_pred = np.sort(f_pred[labels == 0])
-                m_threshold2 = sorted_pred[int(sub_m_pred.shape[0] * 0.82)]
-                f_threshold2 = sorted_pred[int(sub_f_pred.shape[0] * 0.82)]
+                m_pred = pred[m_idx]
+                f_pred = pred[f_idx]
+                sub_m_pred = np.sort(m_pred[labels[m_idx] == 0])
+                sub_f_pred = np.sort(f_pred[labels[f_idx] == 0])
+                m_threshold2 = sub_m_pred[int(sub_m_pred.shape[0] * 0.82)]
+                f_threshold2 = sub_f_pred[int(sub_f_pred.shape[0] * 0.82)]
                 m_min, m_max = min(m_threshold1, m_threshold2), max(m_threshold1, m_threshold2)
                 f_min, f_max = min(f_threshold1, f_threshold2), min(f_threshold1, f_threshold2)
                 thresholds.append([m_min, m_max, f_min, f_max])
@@ -49,23 +55,24 @@ def train_models(data, labels):
             # print("Number of Non Defaulters accepted: ", np.sum(pred[labels == 1] >= threshold), "Number of Defaulters accepted: ", np.sum(pred[labels == 0] >= threshold))
             # print("Number of Non Defaulters denied: ", np.sum(pred[labels == 1] < threshold), "Number of Defaulters denied: ", np.sum(pred[labels == 0] < threshold))
             thresholds.append([threshold] * 4)
-    
     return trained_models, thresholds
 
 def eval_models(models, thresholds, data, labels):
     accuracy = []
     m_accuracy = []
     f_accuracy = []
-    for model, (m_thresh, f_thresh) in zip(models, thresholds):
-        pred = model.predict(data)
-        pred[data['Gender'] == 1] = pred[data['Gender'] == 1] >= m_thresh
-        pred[data['Gender'] == 0] = pred[data['Gender'] == 0] >= f_thresh
-        print(pred)
-        accuracy.append(f1_score(labels, pred))
-        m_accuracy.append(f1_score(labels[data['Gender'] == 1], pred[data['Gender'] == 1]))
-        f_accuracy.append(f1_score(labels[data['Gender'] == 0], pred[data['Gender'] == 0]))
-    print(accuracy)
-    print(m_accuracy)
-    print(f_accuracy)
+    for threshold in thresholds:
+        print(threshold)
+    # for model, (m_thresh, f_thresh) in zip(models, thresholds):
+    #     pred = model.predict(data)
+    #     pred[data['Gender'] == 1] = pred[data['Gender'] == 1] >= m_thresh
+    #     pred[data['Gender'] == 0] = pred[data['Gender'] == 0] >= f_thresh
+    #     print(pred)
+    #     accuracy.append(f1_score(labels, pred))
+    #     m_accuracy.append(f1_score(labels[data['Gender'] == 1], pred[data['Gender'] == 1]))
+    #     f_accuracy.append(f1_score(labels[data['Gender'] == 0], pred[data['Gender'] == 0]))
+    # print(accuracy)
+    # print(m_accuracy)
+    # print(f_accuracy)
     raise Exception("HERE")
     return accuracy
